313eaa50df563120e21938108f4f2016319d96b4
arthur voryshev <what_to_write@mail.ru>
2024-05-21 16:55:31 +1000

    add not faund page

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,15 +1,17 @@
-import './App.css';
+// import '../App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import NotFoundPage from './Errors/NotFoundPage.jsx';
 
 const ChatPage = () => <div>1</div>
 const LoginPage = () => <div>2</div>
-const ErrorPage = () => <div>???</div>
+// const ErrorPage = () => <div></div>
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<ErrorPage />} />
+        <Route path="*" element={<NotFoundPage />} />
         <Route path="/" element={<ChatPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
