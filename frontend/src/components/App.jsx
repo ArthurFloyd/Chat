dc60a43c534e4bb1d223897e5f26a961a4d49420
arthur voryshev <what_to_write@mail.ru>
2024-05-22 23:23:22 +1000

    fix eslint

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -4,17 +4,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NotFoundPage from './Errors/NotFoundPage.jsx';
 import LoginPage from './Login/LoginPage.jsx';
 
+import pathRoutes from '../routes/routes.js';
+
 const ChatPage = () => <div>Тут скоро будет чат</div>
-// const LoginPage = () => <div>2</div>
-// const ErrorPage = () => <div></div>
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<NotFoundPage />} />
-        <Route path="/" element={<ChatPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path={pathRoutes.notFoundPagePath()} element={<NotFoundPage />} />
+        <Route path={pathRoutes.chatPagePath()} element={<ChatPage />} />
+        <Route path={pathRoutes.loginPagePath()} element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
