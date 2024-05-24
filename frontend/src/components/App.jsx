f6cbfe959bde7d83b0f35f10c279f10dcb3532b3
arthur voryshev <what_to_write@mail.ru>
2024-05-24 21:16:32 +1000

    update app

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,5 +1,6 @@
 // import '../App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
 
 import NotFoundPage from './Errors/NotFoundPage.jsx';
 import LoginPage from './Login/LoginPage.jsx';
