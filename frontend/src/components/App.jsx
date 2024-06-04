321dc994c6d19bfd84dbc52e258cf585adbaf8ca
arthur voryshev <what_to_write@mail.ru>
2024-06-04 16:59:27 +1000

    update login page

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -8,6 +8,7 @@ import LoginPage from './Login/LoginPage.jsx';
 import { appRoutes } from '../routes/routes.js';
 
 const ChatPage = () => <div>Тут скоро будет чат</div>;
+const SignupPage = () => <div>Тут скоро будет регистрация</div>;
 
 const App = () => (
   <BrowserRouter>
@@ -15,6 +16,7 @@ const App = () => (
       <Route path={appRoutes.notFoundPagePath()} element={<NotFoundPage />} />
       <Route path={appRoutes.chatPagePath()} element={<ChatPage />} />
       <Route path={appRoutes.loginPagePath()} element={<LoginPage />} />
+      <Route path={appRoutes.signupPagePath()} element={<SignupPage />} />
     </Routes>
   </BrowserRouter>
 );
--- frontend/src/components/Login/LoginPage.jsx
+++ frontend/src/components/Login/LoginPage.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react';
 import { useFormik } from 'formik';
-import Form from 'react-bootstrap/Form';
+import {
+  FloatingLabel, Form, Button, Container, Row, Col, Card, NavLink,
+} from 'react-bootstrap';
 
 import { requestUser } from '../../api.js';
+import { appRoutes } from '../../routes/routes.js';
 
 const LoginPage = () => {
   const formik = useFormik({
@@ -25,31 +28,74 @@ const LoginPage = () => {
   });
 
   useEffect(() => {
-    // const formData = {
-    //   userName: 'admin',
-    //   password: 'admin',
-    // };
 
-    // localStorage =
   });
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <h1 className="text-center mb-4">Войти</h1>
-      <FloatingLabel
-        controlId="floatingInput"
-        label="Email address"
-        className="mb-3"
-      >
-        <Form.Control type="email" placeholder="name@example.com" />
-      </FloatingLabel>
-      <FloatingLabel controlId="floatingPassword" label="Password">
-        <Form.Control type="password" placeholder="Password" />
-      </FloatingLabel>
-
-      <button type="submit">Войти</button>
-    </form>
+    <Container fluid className="h-100">
+      <Row className="justify-content-center align-items-center h-100">
+        <Col xs={12} md={8} xxl={6}>
+          <Card className="shadow-sm">
+            <Card.Body className="p-5 row">
+              <Form>
+                <h1 className="text-center mb-4">Войти</h1>
+                <Form.Group className="form-floating mb-3" controlId="username">
+                  <FloatingLabel controlId="floatingInput" label="Ваш ник" className="mb-3">
+                    <Form.Control type="username" placeholder="name@example.com" />
+                  </FloatingLabel>
+                </Form.Group>
+                <Form.Group className="form-floating mb-4" controlId="password">
+                  <FloatingLabel controlId="floatingPassword" label="Пароль">
+                    <Form.Control type="password" placeholder="Password" />
+                  </FloatingLabel>
+                </Form.Group>
+                <Form.Group className="form-floating mb-4" controlId="">
+                  <FloatingLabel>
+                    <Form.Control.Feedback type="invalid" className="invalid-feedback">
+                      Неверные имя пользователя или пароль
+                    </Form.Control.Feedback>
+                  </FloatingLabel>
+                </Form.Group>
+                <Button type="submit" variant="outline-primary" className="w-100 mb-3">Войти</Button>
+              </Form>
+            </Card.Body>
+            <Card.Footer className="p-4">
+              <div className="text-center text-muted">
+                <span>Нет аккаунта?</span>
+                <NavLink href={appRoutes.signupPagePath()}>Регистрация</NavLink>
+              </div>
+            </Card.Footer>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
+
+  // return (
+  //       <form onSubmit={formik.handleSubmit}>
+  //     <h1 className="text-center mb-4">Войти</h1>
+  //     {/* <label htmlFor="email"></label> */}
+  //     <input
+  //       id="email"
+  //       name="email"
+  //       type="text"
+  //       onChange={formik.handleChange}
+  //       value={formik.values.email}
+  //       placeholder="Ваш ник"
+  //     />
+  //     {/* <label htmlFor="password"></label> */}
+  //     <input
+  //       id="password"
+  //       name="password"
+  //       type="password"
+  //       onChange={formik.handleChange}
+  //       value={formik.values.password}
+  //       placeholder="Пароль"
+  //     />
+
+  //     <button type="submit">Войти</button>
+  //   </form>
+  // )
 };
 
 export default LoginPage;
