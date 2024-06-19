c37852be78215f8be92d46c9f02f56497349520b
arthur voryshev <what_to_write@mail.ru>
2024-06-19 19:34:04 +1000

    create store

--- frontend/src/components/Chat/ChatPage.jsx
+++ frontend/src/components/Chat/ChatPage.jsx
@@ -1,11 +1,18 @@
 import React, { useEffect } from 'react';
-import { requestChannels, requestMessages } from '../../api';
+// import { useDispatch, useSelector } from '@reduxjs/toolkit';
+import { useSelector, useDispatch } from 'react-redux';
+import { requestChannels } from '../../api';
+import { addChannels } from '../../slices/channelsSlice';
 
 const ChatPage = () => {
+  const channels = useSelector((state) => state.channels);
+  console.log(channels);
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    requestChannels().then((data) => console.log('channels', data));
-    requestMessages().then((data) => console.log('messages', data));
-  }, []);
+    requestChannels().then((data) => dispatch(addChannels(data)));
+    // requestMessages().then((data) => console.log('messages', data));
+  }, [dispatch]);
 
   return (
     <div>Тут скоро будет чат</div>
--- frontend/src/slices/channelsSlice.js
+++ frontend/src/slices/channelsSlice.js
@@ -1,15 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  value: 0,
+  channels: [],
 };
 
 const channelsSlice = createSlice({
   name: 'channels',
   initialState,
   reducers: {
+    addChannels: (state, action) => {
+      state.channels.push(...action.payload);
+    },
 
   },
 });
 
+// console.log(channelsSlice.getInitialState());
+
+export const { addChannels } = channelsSlice.actions;
 export default channelsSlice.reducer;
